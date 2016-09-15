7e8ee4b3e3cc1ef07e791432597c60f250314c48
Berzeg07 <berezg86@gmail.com>
2016-09-15 16:39:38 +0300

    15.09

--- wp-content/themes/lantana/single-portfolio.php
+++ wp-content/themes/lantana/single-portfolio.php
@@ -6,13 +6,41 @@
 <section class="main-content">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12 portfolio-wrap">
-				<h1><?php the_title(); ?></h1>
-				<?
-				while ( have_posts() ) : the_post();
-					the_content(); 
-				endwhile;
-				?>
+			<div class="col-md-12">
+				<h1 class="ab-h1-portfolio"><?php the_title(); ?></h1>
+			</div>
+			<div class="clearfix"></div>
+					<?php
+					while ( have_posts() ) : the_post();
+					?>
+			<div class="col-md-6">
+				
+				<?php the_content(); ?> 
+
+					<h3>Детали проекта</h3>
+			
+			
+				<div class="ab-wrap-details">	
+					<div class="ab-col left">
+						<a class="ab-button orange" target="_blank" href="<?php the_field('ссылка_на_верстку'); ?>">Смотреть верстку</a>
+					</div>
+					<div class="ab-col">
+					
+						<span class="ab-platforma">Платформа</span><?php the_field('платформа'); ?>
+					</div>
+					<div class="ab-col">
+						
+						<span class="ab-platforma">Тип проекта</span><?php the_field('тип_проекта'); ?>
+					</div>
+				</div>
+			</div>	
+			<div class="col-md-6">
+				<div class="ab-img-monitor">
+					<?php $image = get_field('скриншот'); ?>
+				<img src="<?php echo $image['url']; ?>" alt="">
+				</div>
+			<?php endwhile; ?>
+		    <?php wp_reset_query(); ?>
 			</div>
 		</div>
 	</div>
