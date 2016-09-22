21c3f18527c33ef93db55f8e5ec24f7229032750
Berzeg07 <berezg86@gmail.com>
2016-09-22 13:24:38 +0300

    21.09

--- wp-content/themes/lantana/single.php
+++ wp-content/themes/lantana/single.php
@@ -15,9 +15,20 @@
 
 				<h1><?php the_title(); ?></h1>
 				<?php while ( have_posts() ) : the_post(); ?>
-				<?php	the_post_thumbnail(); ?>
+				<img src="<?php the_post_thumbnail_url( 'full' ); ?>" alt="" class="img-responsive">
 				<?php the_content(); ?>
-				<?php endwhile;	?>
+				<?php endwhile;	?><br>
+
+				<script type="text/javascript">(function() {
+  if (window.pluso)if (typeof window.pluso.start == "function") return;
+  if (window.ifpluso==undefined) { window.ifpluso = 1;
+    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+  }})();</script>
+<div class="pluso" data-background="transparent" data-options="medium,square,line,horizontal,nocounter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print"></div>
 			</div>
 		</div>
 	</div>
